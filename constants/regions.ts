70c12fba25876a3a188760427bac7ec6ade6cca8
Christopher Díaz <nealthmc@gmail.com>
2022-07-07 12:22:36 -0500

    install redux

---
+++ constants/regions.ts
@@ -0,0 +1,23 @@
+export const regions = {
+    la1: 'lan',
+    la2: 'las',
+    br1: 'br',
+    eun1: 'eun',
+    euw1: 'euw',
+    jp1: 'jp',
+    kr: 'kr',
+    na1: 'na',
+    oc1: 'oc',
+    ru: 'ru',
+    tr1: 'tr',
+
+    lan: 'la1',
+    las: 'la2',
+    br: 'br1',
+    eun: 'eun1',
+    euw: 'euw1',
+    jp: 'jp1',
+    na: 'na1',
+    oc: 'oc1',
+    tr: 'tr1',
+}
